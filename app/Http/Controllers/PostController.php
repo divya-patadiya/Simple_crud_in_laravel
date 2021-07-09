6f8358432ca8e269ad4f08b56bd34ff0ebde573a
divya-patadiya <85231705+divya-patadiya@users.noreply.github.com>
2021-07-09 19:20:29 +0530

    Add files via upload

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Post;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::all();
+
+        return view('post.index')->withPosts($posts);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('post.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $this->validate($request, [
+            'title'         => 'required|min:3|max:255',
+            'description'   => 'required|min:3'
+        ]);
+    
+        Post::create($validatedData);
+    
+        return redirect()->route('post.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return view('post.show')->withPost($post);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return view('post.edit')->withPost($post);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $validatedData = $this->validate($request, [
+            'title'         => 'required|min:3|max:255',
+            'description'   => 'required|min:3'
+          ]);
+    
+          $post->update($validatedData);
+    
+          return redirect()->route('post.show', $post);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $post->delete();
+
+        return redirect()->route('post.index');
+    }
+}
